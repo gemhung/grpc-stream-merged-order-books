2d37063dce0ef40289a1dfe19afcbe3482deb10c
gem hung <gembright.stone.hung@gmail.com>
2022-05-27 09:56:31 +0000

    Update client/src/update.rs, proto/orderbook.proto, server/src/main.rs

--- client/src/update.rs
+++ client/src/update.rs
@@ -1,26 +1,26 @@
 use crate::orderbook::order_book_aggregator_client::OrderBookAggregatorClient;
-use crate::orderbook::{Level, Summary};
+use crate::orderbook::{Data, Level, OrderBook, Summary};
 use tokio::time;
 use tokio::time::Duration;
+use tokio_stream::wrappers::UnboundedReceiverStream;
 use tonic::transport::Channel;
 use tonic::Request;
 use tracing::info;
-use tokio_stream::wrappers::UnboundedReceiverStream;
 
 pub async fn update(mut client: OrderBookAggregatorClient<Channel>) -> Result<(), anyhow::Error> {
     info!("start updating");
     let mut interval = time::interval(Duration::from_millis(1000));
 
-    let mut s = Summary {
-        spread: 1.23,
+    let mut s = OrderBook {
+        exchange: "binance".to_string(),
         bids: vec![
-            Level {
-                exchange: "binance".to_string(),
+            Data {
+                //exchange: "binance".to_string(),
                 price: 1.24,
                 amount: 1.25,
             },
-            Level {
-                exchange: "bitstamp".to_string(),
+            Data {
+                //exchange: "bitstamp".to_string(),
                 price: 1.24,
                 amount: 1.25,
             },
@@ -31,13 +31,14 @@ pub async fn update(mut client: OrderBookAggregatorClient<Channel>) -> Result<()
     let (tx, rx) = tokio::sync::mpsc::unbounded_channel();
 
     let req = Request::new(UnboundedReceiverStream::new(rx));
-    let _res = client.push_summary(req).await?;
+    let _res = client.push_orderbook(req).await?;
 
     loop {
         interval.tick().await;
 
-        s.spread += 1.0;
+        s.bids[0].price += 1.0;
         //let req = Request::new(s.clone());
         tx.send(s.clone())?;
     }
 }
+
--- server/src/main.rs
+++ server/src/main.rs
@@ -3,19 +3,27 @@ pub mod orderbook {
 }
 
 use orderbook::order_book_aggregator_server::{OrderBookAggregator, OrderBookAggregatorServer};
-use orderbook::{Empty, Summary};
+use orderbook::{Empty, Level, OrderBook, Summary};
 use structopt::StructOpt;
 use tokio::sync::broadcast;
 use tokio::sync::mpsc;
+use tokio::sync::oneshot;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
 use tracing::*;
 
-#[derive(Clone)]
+#[derive(Debug)]
+enum Command {
+    UpdateBinance(OrderBook),
+    UpdateBitstamp(OrderBook),
+    Merged(oneshot::Sender<Summary>),
+}
+
+//#[derive(Clone)]
 pub struct Context {
-    //orderbook: Arc<Mutex<HashMap<String, Summary>>,
     broadcast: broadcast::Sender<Summary>,
+    inner_process: mpsc::UnboundedSender<Command>,
 }
 
 #[derive(StructOpt, Debug)]
@@ -28,30 +36,38 @@ struct Opt {
 
 #[tonic::async_trait]
 impl OrderBookAggregator for Context {
-        /*
-    async fn push_summary(&self, request: Request<Summary>) -> Result<Response<Empty>, Status> {
-        let summary = request.into_inner();
-
-        if let Err(err) = self.broadcast.send(summary) {
-            error!(?err);
-        }
-
-        Ok(Response::new(Empty::default()))
-    }
-    */
-
-    async fn push_summary(&self, request: Request<tonic::Streaming<Summary>>) -> Result<Response<Empty>, Status> {
-
+    async fn push_orderbook(
+        &self,
+        request: Request<tonic::Streaming<OrderBook>>,
+    ) -> Result<Response<Empty>, Status> {
         let broadcast = self.broadcast.clone();
-        /*
-            do the merge here
-        */
+
+        let inner_sender = self.inner_process.clone();
         tokio::spawn(async move {
             let mut stream = request.into_inner();
-            while let Some(summary) = stream.message().await? {
-                if let Err(err) = broadcast.send(summary) {
-                    error!(?err);
-                    break;
+            while let Some(orderbook) = stream.message().await? {
+                let (tx, rx) = oneshot::channel();
+
+                let cmd = match orderbook.exchange.as_str() {
+                    "binance" => Command::UpdateBinance(orderbook),
+                    "bitstamp" => Command::UpdateBitstamp(orderbook),
+                    unknown_exchange => {
+                        error!(?unknown_exchange);
+                        continue;
+                    }
+                };
+
+                inner_sender.send(cmd).unwrap();
+                inner_sender.send(Command::Merged(tx)).unwrap();
+
+                let summary = rx.await?;
+
+                if broadcast.receiver_count() > 0 {
+                    if let Err(err) = broadcast.send(summary) {
+                        // this erro is because we broadcasted but there is no active usersa at all
+                        // receiver_count > 0 didn't 100% gurantee to prevent from getting error, so we didn't break here and thus continue
+                        warn!(?err);
+                    }
                 }
             }
 
@@ -63,6 +79,7 @@ impl OrderBookAggregator for Context {
 
     type BookSummaryStream = UnboundedReceiverStream<Result<Summary, Status>>;
 
+    // it's where client booking the merged orderbooks and get a stream for top 10 bids/asks and spread
     async fn book_summary(
         &self,
         request: Request<Empty>,
@@ -75,20 +92,129 @@ impl OrderBookAggregator for Context {
         // this is simply forwarding summary cause we cannot use tokio_stream::wrappers::BroadCastStream
         tokio::spawn(async move {
             loop {
-                if let Err(err) = rx_broadcast
-                    .recv()
-                    .await
-                    .map_err(anyhow::Error::new)
-                    .and_then(|summary| tx.send(Ok(summary)).map_err(anyhow::Error::new))
-                {
-                    warn!(?err);
-                    break;
+                match rx_broadcast.recv().await {
+                    err @ Err(broadcast::error::RecvError::Closed) => {
+                        break err.map_err(anyhow::Error::new)
+                    }
+                    Err(broadcast::error::RecvError::Lagged(frames)) => {
+                        warn!(
+                            "receiving from broadcast is too slow and skipped {} frames",
+                            frames
+                        );
+                    }
+                    Ok(inner) => tx.send(Ok(inner)).map_err(anyhow::Error::new)?,
                 }
             }
         });
 
         Ok(Response::new(UnboundedReceiverStream::new(rx)))
     }
+}
+
+async fn run_inner(mut rx: mpsc::UnboundedReceiver<Command>) -> Result<(), anyhow::Error> {
+    let mut binance = Option::<OrderBook>::None;
+    let mut bitstamp = Option::<OrderBook>::None;
+    let mut merged = Option::<Summary>::None;
+    while let Some(cmd) = rx.recv().await {
+        match cmd {
+            Command::UpdateBinance(book) => {
+                binance = Some(book);
+                merged = None; // indicate it's out-of-dated
+            }
+            Command::UpdateBitstamp(book) => {
+                bitstamp = Some(book);
+                merged = None; // indicate it's out-of-dated
+            }
+            Command::Merged(tx) => {
+                let summary = merged.clone().unwrap_or_else(|| {
+                    //let b
+
+                    Summary {
+                        spread: 1.0,
+                        bids: vec![Level {
+                            exchange: "binance".to_string(),
+                            price: match binance {
+                                Some(ref b) => b.bids.first().map(|data| data.price).unwrap_or(1.0),
+                                None => 1.0,
+                            },
+                            amount: 2.0,
+                        }],
+                        asks: vec![Level {
+                            exchange: "binance".to_string(),
+                            price: 1.0,
+                            amount: 2.0,
+                        }],
+                    }
+                });
+
+                if let Err(err) = tx.send(summary) {
+                    error!(?err);
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn merge_less(v1: &[Level], v2: &[Level]) -> Vec<Level> {
+    let mut iter1 = v1.iter().peekable();
+    let mut iter2 = v2.iter().peekable();
+
+    //assert!(v1.len() + v2.len() <= usize::MAX);
+
+    let mut ret = Vec::with_capacity(v1.len() + v2.len());
+
+    loop {
+        match (iter1.peek(), iter2.peek()) {
+            (Some(Level { price: p1, .. }), Some(Level { price: p2, .. })) if p1 <= p2 => {
+                ret.push(iter1.next().unwrap().clone())
+            }
+
+            (Some(Level { price: p1, .. }), Some(Level { price: p2, .. })) if p1 > p2 => {
+                ret.push(iter2.next().unwrap().clone());
+            }
+            _ => {
+                // one of them has finish iterating all values
+                break;
+            }
+        }
+    }
+
+    ret.extend(iter1.cloned());
+    ret.extend(iter2.cloned());
+
+    ret
+}
+
+fn merge_greater(v1: &[Level], v2: &[Level]) -> Vec<Level> {
+    let mut iter1 = v1.iter().peekable();
+    let mut iter2 = v2.iter().peekable();
+
+    //assert!(v1.len() + v2.len() <= usize::MAX);
+
+    let mut ret = Vec::with_capacity(v1.len() + v2.len());
+
+    loop {
+        match (iter1.peek(), iter2.peek()) {
+            (Some(Level { price: p1, .. }), Some(Level { price: p2, .. })) if p1 >= p2 => {
+                ret.push(iter1.next().unwrap().clone())
+            }
+
+            (Some(Level { price: p1, .. }), Some(Level { price: p2, .. })) if p1 <= p2 => {
+                ret.push(iter2.next().unwrap().clone());
+            }
+            _ => {
+                // one of them has finish iterating all values
+                break;
+            }
+        }
+    }
+
+    ret.extend(iter1.cloned());
+    ret.extend(iter2.cloned());
+
+    ret
 }
 
 #[tokio::main]
@@ -98,33 +224,31 @@ async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::from_args();
     let addr = opt.addr.parse()?;
 
-    let (tx, mut rx): (broadcast::Sender<Summary>, broadcast::Receiver<Summary>) =
-        broadcast::channel(100000);
-    let context = Context { broadcast: tx };
+    let (broadcast_tx, rx) = broadcast::channel(100000);
+    // drop rx so that it won't pending the receiving queue of broadcast
+    drop(rx);
 
-    let handle = tokio::spawn(async move {
-        while let Ok(_) = rx.recv().await.map_err(anyhow::Error::new) {
-            debug!("redundant loop don't care");
-        }
+    let (tx, rx) = mpsc::unbounded_channel();
 
-        info!("exit because sender is closed");
-        Result::<_, anyhow::Error>::Ok(())
-    });
+    let handle = tokio::spawn(run_inner(rx));
 
-    let svc = OrderBookAggregatorServer::new(context);
-    let five_seconds = std::time::Duration::new(5, 0);
+    let context = Context {
+        broadcast: broadcast_tx,
+        inner_process: tx,
+    };
 
     info!("running with addr = {:?} ...", addr);
-
+    let svc = OrderBookAggregatorServer::new(context);
+    let keepalive = std::time::Duration::new(3600, 0);
     let server = Server::builder()
-        .tcp_keepalive(Some(five_seconds.clone()))
-        .http2_keepalive_interval(Some(five_seconds))
+        .tcp_keepalive(Some(keepalive.clone()))
+        .http2_keepalive_interval(Some(keepalive))
         .add_service(svc)
         .serve(addr);
 
     tokio::select! {
         inner = handle => inner?,
-        ret = server => ret.map_err(Into::into),
+        ret = server => ret.map_err(anyhow::Error::new),
         else => return Err(anyhow::anyhow!("all branches in tokio select are disable but didn't catch anything")),
     }?;
 
